120b48bf7eb3b1130a82402e67f79969acc67a20
mattsknobloch <49885138+mattsknobloch@users.noreply.github.com>
2019-06-01 15:32:28 -0400

    add support for leap years to Constants
    add function to check if leaps year.
    add function to check if string is an int
    add constant for leap month days

--- Constants.java
+++ Constants.java
@@ -14,4 +14,46 @@ public final class Constants
 	static String[] shortMonthDays = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", 
 			"11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
 			"21", "22", "23", "24", "25", "26", "27", "28"};
+	static String[] leapYearDays = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", 
+			"11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+			"21", "22", "23", "24", "25", "26", "27", "28", "29"};
+	
+	public static boolean isLeapYear(int year)
+	{
+		if(year % 4 == 0)
+		{
+			if(year % 100 == 0)
+			{
+				if(year % 400 == 0)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return true;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isInteger(String str)
+	{
+		try 
+		{
+			int strAsInt = Integer.parseInt(str);
+			return true;
+		} 
+		catch (Exception e) 
+		{
+			return false;
+		}
+	}
 }
